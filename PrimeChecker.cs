3f588ce887c339746c38792208cab27be0070beb
ImtiazNur <m.imtiaznur@gmail.com>
2021-06-20 19:20:01 -0500

    checked upto square root of the given number

--- PrimeChecker.cs
+++ PrimeChecker.cs
@@ -11,8 +11,10 @@ class PrimeChecker
         if (n <= 1 || n % 2 == 0) {
             return false;
         }
-
-        for (int i = 3; i < n; i+=2) {
+        
+        double limit = Math.Floor(Math.Sqrt(n));
+        
+        for (int i = 3; i < limit; i+=2) {
             if (n % i == 0)
                 return false;
         }
