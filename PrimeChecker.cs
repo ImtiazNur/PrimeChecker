7bc9f92f1b36d267da35f5ce67b9754f035d29f0
ImtiazNur <m.imtiaznur@gmail.com>
2021-06-20 19:17:27 -0500

    checking the odd number divisibility
    All the even numbers are divisible by 2. So checked only odd numbers

--- PrimeChecker.cs
+++ PrimeChecker.cs
@@ -7,12 +7,12 @@ class PrimeChecker
         if (n == 2 || n == 3) {
             return true;
         }
-        
-        if (n <= 1 || n % 2 == 0 || n % 3 == 0) {
+
+        if (n <= 1 || n % 2 == 0) {
             return false;
         }
 
-        for (int i = 4; i < n; i++) {
+        for (int i = 3; i < n; i+=2) {
             if (n % i == 0)
                 return false;
         }
